cb8b925d74ebde0863670517080312bfed188043
Justin Bealer <jmbealer11@gmail.com>
2024-09-22 01:23:01 -0500

    Add .config/hypr/scripts/toolbox/pacman.sh Remove .config/waybar/configs/[BOT & Left] SouthWest Remove .config/waybar/configs/[BOT & Right] SouthEast Remove .config/waybar/configs/[BOT] Camellia Remove .config/waybar/configs/[BOT] Chrysanthemum Remove .config/waybar/configs/[BOT] Default Remove .config/waybar/configs/[BOT] Default Laptop Remove .config/waybar/configs/[BOT] Gardenia Remove .config/waybar/configs/[BOT] Peony Remove .config/waybar/configs/[BOT] Simple Remove .config/waybar/configs/[BOT] Sleek Remove .config/waybar/configs/[Left] WestWing Remove .config/waybar/configs/[Right] EastWing Remove .config/waybar/configs/[TOP & BOT] SummitSplit Remove .config/waybar/configs/[TOP & Left] NorthWest Remove .config/waybar/configs/[TOP & Right] NorthEast Remove .config/waybar/configs/[TOP] Camellia Remove .config/waybar/configs/[TOP] Chrysanthemum Remove .config/waybar/configs/[TOP] Default Remove .config/waybar/configs/[TOP] Default Laptop Remove .config/waybar/configs/[TOP] Default Laptop_v2 Remove .config/waybar/configs/[TOP] Default Laptop_v3 Remove .config/waybar/configs/[TOP] Default_v2 Remove .config/waybar/configs/[TOP] Default_v3 Remove .config/waybar/configs/[TOP] Gardenia Remove .config/waybar/configs/[TOP] Minimal - Long Remove .config/waybar/configs/[TOP] Minimal - Short Remove .config/waybar/configs/[TOP] Peony Remove .config/waybar/configs/[TOP] Simple Remove .config/waybar/configs/[TOP] Sleek Remove .config/waybar/modules Remove .config/waybar/style/[Black & White] Monochrome.css Remove .config/waybar/style/[Catppuccin] Latte.css Remove .config/waybar/style/[Catppuccin] Mocha.css Remove .config/waybar/style/[Colored] Chroma Glow.css Remove .config/waybar/style/[Colored] Translucent.css Remove .config/waybar/style/[Colorful] Aurora Blossom.css Remove .config/waybar/style/[Colorful] Aurora.css Remove .config/waybar/style/[Colorful] Rainbow Spectrum.css Remove .config/waybar/style/[Dark] Golden Noir.css Remove .config/waybar/style/[Dark] Half-Moon.css Remove .config/waybar/style/[Dark] Latte-Wallust combined.css Remove .config/waybar/style/[Dark] Purpl.css Remove .config/waybar/style/[Dark] Wallust Obsidian Edge.css Remove .config/waybar/style/[Extra] Crimson.css Remove .config/waybar/style/[Extra] Mauve.css Remove .config/waybar/style/[Extra] Rose Pine.css Remove .config/waybar/style/[Extra] Simple Pink.css Remove .config/waybar/style/[Light] Monochrome Contrast.css Remove .config/waybar/style/[Light] Obsidian Glow.css Remove .config/waybar/style/[Retro] Simple Style.css Remove .config/waybar/style/[Transparent] Crystal Clear.css Remove .config/waybar/style/[Wallust Bordered] Chroma Fusion Edge.css Remove .config/waybar/style/[Wallust Bordered] Chroma Simple.css Remove .config/waybar/style/[Wallust Transparent] Crystal Clear.css Remove .config/waybar/style/[Wallust] Box type.css Remove .config/waybar/style/[Wallust] Chroma Edge.css Remove .config/waybar/style/[Wallust] Chroma Fusion.css Remove .config/waybar/style/[Wallust] Chroma Tally.css Remove .config/waybar/style/[Wallust] Colored.css Remove .config/waybar/style/[Wallust] Simple.css Remove .config/waybar/style/catppuccin-themes/latte.css Remove .config/waybar/style/catppuccin-themes/mocha.css Remove .config/waybar/config Remove .config/waybar/style.css Remove .config/waybar/wallust/colors-waybar.css Remove .config/wofi/config Remove .config/wofi/style.css

---
+++ dot_config/hypr/scripts/toolbox/pacman.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#  _   _           _       _
+# | | | |_ __   __| | __ _| |_ ___  ___
+# | | | | '_ \ / _` |/ _` | __/ _ \/ __|
+# | |_| | |_) | (_| | (_| | ||  __/\__ \
+#  \___/| .__/ \__,_|\__,_|\__\___||___/
+#       |_|
+# -----------------------------------------------------
+# Requires pacman-contrib, aur
+
+# Added a space in text and shorten it
+function stringToLen() {
+  STRING="$1"
+  LEN="$2"
+  if [ ${#STRING} -gt "$LEN" ]; then
+    printf "%-21s" "${STRING:0:$((LEN - 2))}..."
+  else
+    printf "%-20s" "$STRING"
+  fi
+}
+
+# Get list packages
+function packages() {
+  checkupdates --nocolor
+  pacman -Qm | aur vercmp
+}
+# -----------------------------------------------------
+
+# Check updates
+if [ "$1" == "check" ]; then
+  mapfile -t updates < <(packages)
+  updated=${#updates[@]}
+  total="$(pacman -Q | wc -l)"
+  text=" ${updated} | 󰏖 $(pacman -Q | wc -l)"
+  tooltip=""
+
+  if [ "$updated" -ge 1 ]; then
+    tooltip=" <b>$updated updates | 󰏖 Packages $total</b>\n"
+    tooltip+="<b>$(stringToLen "Package" 20) $(stringToLen "Current" 20) $(stringToLen "Next" 20)</b>\n"
+
+    for i in "${updates[@]}"; do
+      update="$(stringToLen "$(echo "$i" | awk '{print $1}')" 20)"
+      prev="$(stringToLen "$(echo "$i" | awk '{print $2}')" 20)"
+      next="$(stringToLen "$(echo "$i" | awk '{print $4}')" 20)"
+
+      tooltip+="<b>$update</b> $prev $next\n"
+    done
+
+    tooltip=${tooltip::-2}
+  else
+    tooltip=" <b>$updated updates | 󰏖 Packages $total</b>\n- Packages not found for update"
+  fi
+
+  cat <<EOF
+{ "text":"$text", "tooltip":"$tooltip"}
+EOF
+fi
